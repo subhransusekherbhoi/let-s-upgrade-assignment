5e4820d3c70a508cc53f0aba50b34cbe2144078b
subhransusekherbhoi <66049375+subhransusekherbhoi@users.noreply.github.com>
2021-01-20 19:24:11 +0530

    Add files via upload

---
+++ let's upgrade/assignment 1/factorial.py
@@ -0,0 +1,10 @@
+num=5
+f=1
+if num < 0:
+   print("Sorry, factorial does not exist")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:   
+    for i in range(1,num+1):
+     f=f*i
+print(f)    
